514a970860c25cab2ec05305aa2b2bdd286be30c
Chacha <anne-charlotte.jeanvoine@laposte.net>
2021-10-12 14:08:39 +0200

    mise en page avec css

--- principal.js
+++ principal.js
@@ -1,23 +1,6 @@
-
-
 //Générer un nombre aléatoire entre 1 et 100.
-//Fournir au joueur le moyen de saisir un nombre.
-//Vérifier si le nombre saisi par le joueur est correct.
-//S'il est correct :
-//Afficher un message de félicitations.
-//Empêcher que le joueur saisisse de nouveau un nombre.
-//Afficher un contrôle pour que le joueur puisse rejouer.
-//S'il est faux et que le joueur a encore des tours à jouer :
-//Informer le joueur que sa proposition de nombre est fausse.
-//Lui permettre d'entrer une nouvelle proposition de nombre.
-//Incrémenter le nombre de tours de 1.
-//S'il est faux et que le joueur n'a plus de tours à jouer :
-//Informer le joueur qu'il a perdu et que la partie est finie.
-//Empêcher que le joueur saisisse de nouveau un nombre.
-//Afficher un contrôle pour que le joueur puisse rejouer.
-//Une fois le jeu redémarré, s'assurer que la logique du jeu et l'interface utilisateur sont complètement réinitialisées, puis revenir à l'étape 1
-
 let randomNumber = Math.floor(Math.random() * 100) + 1;
+    //Fournir au joueur le moyen de saisir un nombre.
     let guesses = document.querySelector('.guesses');
     let lastResult = document.querySelector('.lastResult');
     let lowOrHi = document.querySelector('.lowOrHi');
@@ -26,6 +9,7 @@ let randomNumber = Math.floor(Math.random() * 100) + 1;
     let guessCount = 1;
     let resetButton;
 
+    //Vérifier si le nombre saisi par le joueur est correct.
     function checkGuess() {
       let userGuess = Number(guessField.value);
       if (guessCount === 1) {
@@ -34,31 +18,43 @@ let randomNumber = Math.floor(Math.random() * 100) + 1;
 
       guesses.textContent += userGuess + ' ';
 
+      //S'il est correct :
+      //Afficher un message de félicitations.
       if (userGuess === randomNumber) {
         lastResult.textContent = 'Bravo, vous avez trouvé le nombre !';
         lastResult.style.backgroundColor = 'green';
         lowOrHi.textContent = '';
+        //Empêcher que le joueur saisisse de nouveau un nombre.
         setGameOver();
+        //Incrémenter le nombre de tours de 1.
+        //S'il est faux et que le joueur n'a plus de tours à jouer :
+        //Informer le joueur qu'il a perdu et que la partie est finie.
+        //Empêcher que le joueur saisisse de nouveau un nombre.
       } else if (guessCount === 10) {
-        lastResult.textContent = '!!! PERDU !!!';
+        lastResult.textContent = '!!! GAME OVER !!!';
+        lastResult.style.backgroundColor = 'red';
         lowOrHi.textContent = '';
         setGameOver();
+        
+        //Lui permettre d'entrer une nouvelle proposition de nombre.
       } else {
         lastResult.textContent = 'Faux!';
-        lastResult.style.backgroundColor = 'red';
+        lastResult.style.backgroundColor = 'orange';
         if (userGuess < randomNumber) {
           lowOrHi.textContent='Le nombre saisi est trop petit !' ;
         } else if(userGuess > randomNumber) {
           lowOrHi.textContent = 'Le nombre saisi est trop grand!';
         }
       }
-
+      //S'il est faux et que le joueur a encore des tours à jouer :
+      //Informer le joueur que sa proposition de nombre est fausse.
       guessCount++;
       guessField.value = '';
     }
 
     guessSubmit.addEventListener('click', checkGuess);
 
+     //Afficher un contrôle pour que le joueur puisse rejouer.
     function setGameOver() {
       guessField.disabled = true;
       guessSubmit.disabled = true;
@@ -67,14 +63,15 @@ let randomNumber = Math.floor(Math.random() * 100) + 1;
       document.body.appendChild(resetButton);
       resetButton.addEventListener('click', resetGame);
     }
-
+    //Afficher un contrôle pour que le joueur puisse rejouer.
     function resetGame() {
       guessCount = 1;
       let resetParas = document.querySelectorAll('.resultParas p');
       for (let i = 0 ; i < resetParas.length ; i++) {
         resetParas[i].textContent='';
       }
-
+      
+    //Une fois le jeu redémarré, s'assurer que la logique du jeu et l'interface utilisateur sont complètement réinitialisées, puis revenir à l'étape 1
       resetButton.parentNode.removeChild(resetButton);
       guessField.disabled = false;
       guessSubmit.disabled = false;
